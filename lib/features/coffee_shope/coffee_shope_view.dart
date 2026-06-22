a33d808b2efe74c6c02f9cdcebdba7216e8b58bc
Junaid Jameel <junaidjameel396@gmail.com>
2026-06-22 12:24:11 +0500

    Coffee App UI

---
+++ lib/features/coffee_shope/coffee_shope_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CoffeeShopeView extends StatelessWidget {
+  const CoffeeShopeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
