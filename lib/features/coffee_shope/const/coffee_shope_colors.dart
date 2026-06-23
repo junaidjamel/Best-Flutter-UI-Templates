c937809176c793b690d8cada62dda07703588460
Junaid Jameel <junaidjameel396@gmail.com>
2026-06-23 10:26:52 +0500

    UI

---
+++ lib/features/coffee_shope/const/coffee_shope_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CoffeeShopeColors {
+  static const whiteClr = Colors.white;
+  static const primaryBlack = Color(0xff0C1014);
+  static const lightOrange = Color(0xffcf7741);
+  static const greyClr = Color(0xff515354);
+}
