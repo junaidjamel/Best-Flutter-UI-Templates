7b25835c4e744cbc7b5f3e0176a024d0a96341ca
Junaid Jameel <junaidjameel396@gmail.com>
2026-07-03 08:46:52 +0500

    UI

---
+++ lib/features/subscription_tracker/subscription_tracker_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SubscriptionTrackerView extends StatelessWidget {
+  const SubscriptionTrackerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
